e60c87b1a62e22e24366aa0f1a65e8c2dcceffdc
programmiererr-blog <azimovumidbekgz@mail.com>
2024-05-28 15:49:58 +0500

    section1 yaratildi

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ const navbar={
     login:"login",
     signup:"signup",
     logout:"logout"
+    
+}
+baza1={
+    img1:'jpg/bir.jpg',
+    name:"Ali Alimov",
+    surname:"@alimardon",
+    email:"alimardon@gamil.com",
+    phoneNomber:"(77) 777 99 88",
+    cytiy:"Tashkent"
 }
 
 const logs = document.querySelector('.logs'),
-    buttons = document.querySelector('.buttons');
+    buttons = document.querySelector('.buttons'),
+    section1 = document.querySelector('.section1');
 
    logs.innerHTML=`
    <a href="#">${navbar.logo}</a>
@@ -21,5 +31,16 @@ const logs = document.querySelector('.logs'),
    <a href="#">${navbar.login}</a>
    <a href="#">${navbar.signup}</a>
    <a href="#">${navbar.logout}</a>
+   
+   `
+
+   section1.innerHTML=`
+   <img src="${baza1.img1}" alt="About">
+   <h1>${baza1.name}</h1>
+   <h2>${baza1.surname}</h2>
+   <p>${baza1.email}</p>
+   <p>${baza1.phoneNomber}</p>  
+   <p>${baza1.cytiy}</p>
+   
    
    `
